2030e7b7a4eee67ca29ee63968959ea23580c737
hfan36 <hfan36@gmail.com>
2014-08-20 13:42:11 -0700

    use it to generate some data for reconstruction, hopefully it works

--- temp_fix_data_files.m
+++ temp_fix_data_files.m
@@ -4,45 +4,34 @@ clc
 
 %% read data file
 
-%switch directory
-% data_directory = 'H:\CT data\051314\cleaned_data';
-% cd(data_directory);
-
-image_data = readBinary('H:\CT data\051314\cleaned_data\clean_phantom_60kV_400uA_10sec_0.ct', 2160*2560, 'float');
-image_data = reshape(image_data, 2560, 2160);
-figure; imagesc(image_data); colormap gray; axis equal; colorbar;
-
-
-
-
-
-small_image_data = readBinary('H:\CT data\051314\cleaned_data\C1024_clean_phantom_60kV_400uA_10sec_0.ct', 1024*1024, 'float');
-small_image_data = reshape(small_image_data, 1024, 1024);
-figure; imagesc(small_image_data'); colormap gray; axis equal; colorbar; title('center before');
-
-
-image_data2 = image_data(769:769+1023, 569:569+1023);
-figure; imagesc(10 - log(image_data2)); colormap gray; axis equal; colorbar; title('centered today');
-
-
+% load average flood data 
 load('H:\CT data\051514\cleaned_data\clean_mean_variance.mat');
 
-%     t = (log(flood_mean) - log(dat));
-%     t(t < 0.08) = 0;
-t = log(flood_mean) - log(image_data(:));
-t(t < 0.08) = 0;
+% This section is used to clip the clean phantom images to a certain size,
+% subtract flood correction and use them to test the reconstruction
+% algorithm, or actually test to see how the reconstruction looks like
+% because I think the actual reconstruction algorithm works just fine....
+% for n = 1:180
+%    image_data_folder = 'H:\CT data\051314\cleaned_data\';
+%    image_data_filename = strcat('clean_phantom_60kV_400uA_10sec_', num2str(n-1), '.ct');
+%    image_data = readBinary(strcat(image_data_folder, image_data_filename), 2160*2560, 'float');
+%    flood_corrected = log(flood_mean) - log(image_data);
+%    flood_corrected (flood_corrected < 0.08) = 0;
+%    flood_corrected = reshape(flood_corrected, 2560, 2160);
+%    dat = flood_corrected(769:769+1023, 569:569+1023);
+%    
+%    save_file_name = strcat(image_data_folder, 'C1024_2_', image_data_filename);
+%    writeBinary(dat(:), save_file_name, 'float');
+% end
 
+% figure; imagesc(reshape(dat, 1024, 1024)); colormap gray; axis equal;
 
-
-
+%% just looking at the simulation images
 sim_image_data = readBinary('H:\CT data\051314\simulation\fp_phantom_voxelvolume_10.bin', 1024*1024, 'float');
 sim_image_data = reshape(sim_image_data, 1024, 1024);
-figure; imagesc(sim_image_data'); colormap gray; axis equal; colorbar; title('simulation with calibration parameters');
+% figure; imagesc(sim_image_data'); colormap gray; axis equal; colorbar; title('simulation with calibration parameters');
 
 %% read phantom voxelvolume
 phantom = readBinary('H:\CT data\051314\simulation\phantom_voxelvolume.bin', 64*64*64, 'float');
 phantom = reshape(phantom, 64, 64, 64);
-
 % figure; imagesc(phantom(:,:,32)); colormap gray; axis equal; colorbar;
-
-% cd ..
